a1ba9c98a53fdfb97d8a757a0701d524fa0c6129
Shashika Jayawardhana <shashikaj@zone24x7.com>
2024-08-06 10:52:06 +0530

    Implement JWT authentication and authorization.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [AuthModule, UsersModule, HealthModule],
 })
 export class AppModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User, UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService,
+    ) {}
+
+    async signIn(
+        username: string,
+        pass: string,
+    ): Promise<{ access_token: string }> {
+        const user = await this.usersService.findOne(username);
+
+        if (user?.password !== pass) {
+            throw new UnauthorizedException();
+        }
+
+        const payload = { sub: user.userId, username: user.username };
+
+        return {
+            access_token: this.jwtService.sign(payload),
+        }
+    }
+}
