d124de6c5db6f372bf51320f8c0621118f9ac400
Shashika Jayawardhana <shashikaj@zone24x7.com>
2024-08-23 14:28:37 +0530

    Setup database module.

---
+++ src/repository/database.providers.ts
@@ -0,0 +1,20 @@
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+    {
+        provide: 'DATA_SOURCE',
+        useFactory: async () => {
+            const dataSource = new DataSource({
+                type: 'mysql',
+                host: 'localhost',
+                port: 3306,
+                username: 'root',
+                password: 'root',
+                database: 'ecommerce',
+                entities: [],
+            });
+
+            return dataSource.initialize();
+        },
+    },
+];
